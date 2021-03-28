7db71ac2322bb1b7547bc527ca7b8c0b115c2466
TheRealKizu <therealkizu@protonmail.com>
2021-03-28 15:48:56 +0800

    chore: now using 7 workspaces

--- cfg/polybar/launch.sh
+++ cfg/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,4 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch all bars :D
-polybar -c $HOME/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini main &
