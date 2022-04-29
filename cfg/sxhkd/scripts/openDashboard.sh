d56b2ca8a41c5eda85d262c077bdc1fdf41cfc81
Jan Leigh Muñoz <janleigh@pm.me>
2022-04-29 18:20:28 +0800

    fix: dashboard opening on middle click while on a fullscreen app

--- cfg/sxhkd/scripts/openDashboard.sh
+++ cfg/sxhkd/scripts/openDashboard.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ -z "$(bspc query -N -n .local.focused.\!fullscreen)" ]; then
+if [ -z "$(bspc query -N -n .local.focused)" ]; then
     sh $HOME/.config/eww/scripts/openDashboard.sh
 fi
