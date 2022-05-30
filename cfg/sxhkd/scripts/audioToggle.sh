da63ff89bda319b2f5ea3c83c7c95c73daa44708
Jan Leigh Muñoz <janleigh@pm.me>
2022-05-30 13:58:44 +0800

    feat: reorganized sxhkd config

--- cfg/sxhkd/scripts/audioToggle.sh
+++ cfg/sxhkd/scripts/audioToggle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [[ -z $(amixer get Master | grep off) ]]; then
+    notify-send -i "$HOME/.icons/Zafiro-Icons-Dark/panel/22/audio-volume-muted.svg" "Audio Muted"
     mpv --keep-open=no --really-quiet "$HOME/.config/sxhkd/assets/audioOff.wav"
     amixer set Master toggle
 else
     amixer set Master toggle
+    notify-send -i "$HOME/.icons/Zafiro-Icons-Dark/panel/22/audio-volume-high.svg" "Audio On"
     mpv --keep-open=no --really-quiet "$HOME/.config/sxhkd/assets/audioOn.wav"
 fi
--- cfg/sxhkd/scripts/openDashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -z "$(bspc query -N -n .local.focused)" ]; then
-    sh $HOME/.config/eww/scripts/openDashboard.sh
-fi
